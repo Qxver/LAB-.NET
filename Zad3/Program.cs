4be9e15c857bc8b349308b9b7bc70350842cb890
Wojciech Paradziński <84313530+Qxver@users.noreply.github.com>
2026-03-10 14:23:54 +0100

    Add files via upload

---
+++ Zad3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Parametry wejściowe");
+
+        if (args.Length == 0) {
+            Console.WriteLine("Brak parametrów.");
+        }
+        else {
+            for (int i = 0; i < args.Length; i++) {
+                Console.WriteLine($"Parametr {i}: {args[i]}");
+            }
+        }
+
+        Console.WriteLine("\n=== Wybrane zmienne środowiskowe ===");
+
+        string path = Environment.GetEnvironmentVariable("PATH");
+        string user = Environment.GetEnvironmentVariable("USERNAME");
+
+        Console.WriteLine($"USERNAME: {user}");
+        Console.WriteLine($"PATH: {path}");
+
+        Console.WriteLine("\n=== Wszystkie zmienne środowiskowe ===");
+
+        var variables = Environment.GetEnvironmentVariables();
+
+        foreach (var key in variables.Keys) {
+            Console.WriteLine($"{key} = {variables[key]}");
+        }
+    }
+}
---
+++ Zad5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        if (args.Length != 2) {
+            Console.Error.WriteLine("Użycie: progZ4 słowo1 słowo2");
+            return;
+        }
+
+        string word1 = args[0];
+        string word2 = args[1];
+
+        string line;
+        
+        while ((line = Console.ReadLine()) != null) {
+            string modified = line.Replace(word1, word2);
+            Console.WriteLine(modified);
+        }
+    }
+}
