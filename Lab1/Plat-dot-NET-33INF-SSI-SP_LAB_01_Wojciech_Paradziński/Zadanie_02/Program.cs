a497cceea04317275065f7311afe4cb3d7c0368e
George <167248869+GeorgeKsr@users.noreply.github.com>
2026-03-12 17:25:04 +0100

    Zad2 Rozszerzenie

--- Lab1/Plat-dot-NET-33INF-SSI-SP_LAB_01_Wojciech_Paradziński/Zadanie_02/Program.cs
+++ Lab1/Plat-dot-NET-33INF-SSI-SP_LAB_01_Wojciech_Paradziński/Zadanie_02/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+// Rozszerzenie - wybór języka i wyświetlenie odpowiedniego powitania
+
+Console.WriteLine("Wybierz język:");
+Console.WriteLine("1. Polski");
+Console.WriteLine("2. Angielski");
+Console.WriteLine("3. Niemiecki");
+
+Console.Write("Twój wybór:");
+string choice = Console.ReadLine();
+
+switch (choice)
+{
+    case "1":
+        Console.WriteLine("Witaj, Świecie!");
+        break;
+    case "2":
+        Console.WriteLine("Hello, World!");
+        break;
+    case "3":
+        Console.WriteLine("Hallo, Welt!");
+        break;
+    default:
+        Console.WriteLine("Nieznany wybór");
+        break;
+}
