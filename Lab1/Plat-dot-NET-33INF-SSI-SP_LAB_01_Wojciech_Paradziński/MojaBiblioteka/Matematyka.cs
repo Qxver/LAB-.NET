5f85e773d9507d5c6a772ba225f6cc9a0dcc4138
George <167248869+GeorgeKsr@users.noreply.github.com>
2026-03-13 18:51:23 +0100

    Zadanie06

---
+++ Lab1/Plat-dot-NET-33INF-SSI-SP_LAB_01_Wojciech_Paradziński/MojaBiblioteka/Matematyka.cs
@@ -0,0 +1,15 @@
+﻿namespace MojaBiblioteka
+{
+    public class Matematyka
+    {
+        public double ObliczPotege(double liczba, double wykladnik)
+        {
+            return System.Math.Pow(liczba, wykladnik);
+        }
+
+        public string PobierzInfo()
+        {
+            return "Biblioteka Matematyczna v1.0 - stworzona dla Zadania 06.";
+        }
+    }
+}
---
+++ Lab1/Plat-dot-NET-33INF-SSI-SP_LAB_01_Wojciech_Paradziński/Zadanie_06/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MojaBiblioteka; 
+
+Console.WriteLine("Program konsolowy Z użyciem DLL");
+
+Matematyka kalkulator = new Matematyka();
+
+string info = kalkulator.PobierzInfo();
+Console.WriteLine($"Info z DLL: {info}\n");
+
+Console.Write("Podaj liczbę: ");
+if (double.TryParse(Console.ReadLine(), out double liczba))
+{
+    Console.Write("Podaj potęgę: ");
+    if (double.TryParse(Console.ReadLine(), out double potega))
+    {
+        double wynik = kalkulator.ObliczPotege(liczba, potega);
+        Console.WriteLine($"\nWynik obliczony w bibliotece: {wynik}");
+    }
+    else
+    {
+        Console.WriteLine("Błędna potęga.");
+    }
+}
+else
+{
+    Console.WriteLine("Błędna liczba.");
+}
+
+Console.WriteLine("\nNaciśnij dowolny klawisz, aby zakończyć...");
+Console.ReadKey();
