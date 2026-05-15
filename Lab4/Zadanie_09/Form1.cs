fac1319d98eab57064fe9bab9ab2be9d6d24ff19
Wojciech Paradziński <84313530+Qxver@users.noreply.github.com>
2026-05-15 08:44:45 +0200

    Add files via upload

---
+++ Lab4/Zadanie_09/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing; // Wymagane do drukowania
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;         // Wymagane do obsługi plików XML
+
+namespace Zadanie_09
+{
+    public partial class Form1 : Form
+    {
+        // Deklaracja naszych kontrolek
+        private ListView listView;
+        private Button btnLoadXml;
+        private Button btnPrintPreview;
+        private Button btnPrint;
+
+        // Komponenty do drukowania
+        private PrintDocument printDoc;
+        private PrintPreviewDialog printPreviewDialog;
+
+        // Zmienne pomocnicze do drukowania
+        private List<ListViewItem> itemsToPrint;
+        private int currentItemIndex;
+
+        public Form1()
+        {
+            InitializeComponent(); // Twoja standardowa metoda (zostawiamy ją)
+
+            // Wywołujemy nasze metody konfiguracyjne
+            InitializeCustomUI();
+            InitializePrinting();
+        }
+
+        // --- KONFIGURACJA INTERFEJSU ---
+        private void InitializeCustomUI()
+        {
+            this.Text = "Zadanie 09 - Druk XML";
+            this.Size = new Size(500, 400);
+
+            // Konfiguracja ListView
+            listView = new ListView
+            {
+                View = View.Details,
+                FullRowSelect = true,
+                GridLines = true,
+                Location = new Point(12, 12),
+                Size = new Size(460, 250),
+                Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right
+            };
+            listView.Columns.Add("Nazwa Produktu", 250);
+            listView.Columns.Add("Cena (PLN)", 150);
+
+            // Przycisk: Wczytaj XML
+            btnLoadXml = new Button { Text = "Wczytaj XML", Location = new Point(12, 280), Size = new Size(100, 40), Anchor = AnchorStyles.Bottom | AnchorStyles.Left };
+            btnLoadXml.Click += BtnLoadXml_Click;
+
+            // Przycisk: Podgląd wydruku
+            btnPrintPreview = new Button { Text = "Podgląd wydruku", Location = new Point(236, 280), Size = new Size(120, 40), Anchor = AnchorStyles.Bottom | AnchorStyles.Right };
+            btnPrintPreview.Click += BtnPrintPreview_Click;
+
+            // Przycisk: Drukuj
+            btnPrint = new Button { Text = "Drukuj", Location = new Point(372, 280), Size = new Size(100, 40), Anchor = AnchorStyles.Bottom | AnchorStyles.Right };
+            btnPrint.Click += BtnPrint_Click;
+
+            // Dodanie kontrolek do okna
+            this.Controls.Add(listView);
+            this.Controls.Add(btnLoadXml);
+            this.Controls.Add(btnPrintPreview);
+            this.Controls.Add(btnPrint);
+        }
+
+        // --- KONFIGURACJA DRUKOWANIA ---
+        private void InitializePrinting()
+        {
+            printDoc = new PrintDocument();
+            printDoc.BeginPrint += PrintDoc_BeginPrint;
+            printDoc.PrintPage += PrintDoc_PrintPage;
+
+            printPreviewDialog = new PrintPreviewDialog
+            {
+                Document = printDoc,
+                Width = 600,
+                Height = 600,
+                ShowIcon = false,
+                Text = "Podgląd wydruku"
+            };
+        }
+
+        // --- ZDARZENIA PRZYCISKÓW ---
+        private void BtnLoadXml_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listView.Items.Clear();
+                // Wczytanie pliku XML
+                XDocument doc = XDocument.Load("dane.xml");
+
+                foreach (XElement element in doc.Descendants("Produkt"))
+                {
+                    string nazwa = element.Attribute("Nazwa")?.Value;
+                    string cena = element.Attribute("Cena")?.Value;
+
+                    ListViewItem item = new ListViewItem(nazwa);
+                    item.SubItems.Add(cena);
+                    listView.Items.Add(item);
+                }
+                MessageBox.Show("Dane zostały załadowane.", "Sukces", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Błąd: Upewnij się, że plik dane.xml istnieje w folderze z aplikacją (bin/Debug).\n\nSzczegóły: " + ex.Message, "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BtnPrintPreview_Click(object sender, EventArgs e)
+        {
+            if (listView.Items.Count == 0) return;
+            printPreviewDialog.ShowDialog();
+        }
+
+        private void BtnPrint_Click(object sender, EventArgs e)
+        {
+            if (listView.Items.Count == 0) return;
+            printDoc.Print();
+        }
+
+        // --- LOGIKA DRUKOWANIA ---
+        private void PrintDoc_BeginPrint(object sender, PrintEventArgs e)
+        {
+            itemsToPrint = new List<ListViewItem>();
+
+            // Logika wyboru: zaznaczone vs wszystkie
+            if (listView.SelectedItems.Count > 0)
+            {
+                foreach (ListViewItem item in listView.SelectedItems)
+                    itemsToPrint.Add(item);
+            }
+            else
+            {
+                foreach (ListViewItem item in listView.Items)
+                    itemsToPrint.Add(item);
+            }
+
+            currentItemIndex = 0;
+        }
+
+        private void PrintDoc_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Font titleFont = new Font("Arial", 16, FontStyle.Bold);
+            Font regularFont = new Font("Arial", 12, FontStyle.Regular);
+            SolidBrush brush = new SolidBrush(Color.Black);
+
+            float yPos = e.MarginBounds.Top;
+            float leftMargin = e.MarginBounds.Left;
+
+            if (currentItemIndex == 0)
+            {
+                g.DrawString("Raport XML z Zadania 09", titleFont, brush, leftMargin, yPos);
+                yPos += titleFont.GetHeight(g) + 20;
+            }
+
+            while (currentItemIndex < itemsToPrint.Count)
+            {
+                ListViewItem item = itemsToPrint[currentItemIndex];
+                string textToPrint = $"- {item.Text} | Cena: {item.SubItems[1].Text} PLN";
+
+                g.DrawString(textToPrint, regularFont, brush, leftMargin, yPos);
+                yPos += regularFont.GetHeight(g) + 10;
+
+                currentItemIndex++;
+
+                if (yPos > e.MarginBounds.Bottom)
+                {
+                    e.HasMorePages = true;
+                    return;
+                }
+            }
+
+            e.HasMorePages = false;
+        }
+    }
+}
---
+++ Lab4/Zadanie_10/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Zadanie_10
+{
+    public partial class Form1 : Form
+    {
+        // Komponenty menu (stworzymy je w kodzie)
+        private MenuStrip mainMenuStrip;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 1. Ustawienie tego okna jako rodzica MDI
+            this.IsMdiContainer = true;
+            this.Text = "Przeglądarka Obrazów MDI (XML + GIF)";
+            this.WindowState = FormWindowState.Maximized;
+
+            // 2. Inicjalizacja menu
+            SetupMenu();
+        }
+
+        private void SetupMenu()
+        {
+            mainMenuStrip = new MenuStrip();
+
+            // Menu PLIK
+            ToolStripMenuItem fileMenu = new ToolStripMenuItem("Plik");
+            fileMenu.DropDownItems.Add("Wczytaj Galerię XML", null, OpenXml_Click);
+            fileMenu.DropDownItems.Add("Otwórz pojedynczy plik", null, OpenSingle_Click);
+            fileMenu.DropDownItems.Add(new ToolStripSeparator());
+            fileMenu.DropDownItems.Add("Wyjście", null, (s, e) => Application.Exit());
+
+            // Menu OKNA (do zarządzania widokiem)
+            ToolStripMenuItem windowsMenu = new ToolStripMenuItem("Okna");
+            windowsMenu.DropDownItems.Add("Kaskada", null, (s, e) => this.LayoutMdi(MdiLayout.Cascade));
+            windowsMenu.DropDownItems.Add("Pionowo", null, (s, e) => this.LayoutMdi(MdiLayout.TileVertical));
+            windowsMenu.DropDownItems.Add("Poziomo", null, (s, e) => this.LayoutMdi(MdiLayout.TileHorizontal));
+
+            mainMenuStrip.Items.Add(fileMenu);
+            mainMenuStrip.Items.Add(windowsMenu);
+
+            this.MainMenuStrip = mainMenuStrip;
+            this.Controls.Add(mainMenuStrip);
+        }
+
+        // --- OBSŁUGA ZDARZEŃ ---
+
+        private void OpenXml_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                Filter = "Pliki XML (*.xml)|*.xml",
+                Title = "Wybierz plik konfiguracyjny galerii"
+            };
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                LoadImagesFromXml(ofd.FileName);
+            }
+        }
+
+        private void OpenSingle_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                // Dodaliśmy obsługę gifa!
+                Filter = "Obrazy|*.jpg;*.jpeg;*.png;*.bmp;*.gif",
+                Title = "Wybierz obrazek"
+            };
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                CreateChildForm(ofd.FileName, Path.GetFileName(ofd.FileName));
+            }
+        }
+
+        // --- LOGIKA XML I MDI ---
+
+        private void LoadImagesFromXml(string path)
+        {
+            try
+            {
+                XDocument doc = XDocument.Load(path);
+                var zdjecia = doc.Descendants("Zdjecie");
+
+                foreach (var z in zdjecia)
+                {
+                    string sPath = z.Attribute("Sciezka")?.Value;
+                    string sTytul = z.Attribute("Tytul")?.Value ?? "Obraz";
+
+                    if (File.Exists(sPath))
+                    {
+                        CreateChildForm(sPath, sTytul);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Błąd XML: " + ex.Message);
+            }
+        }
+
+        private void CreateChildForm(string imagePath, string title)
+        {
+            // Tworzymy nowe okno podrzędne
+            Form child = new Form();
+            child.MdiParent = this; // To czyni okno częścią MDI
+            child.Text = title;
+            child.Size = new Size(500, 400);
+
+            // Dodajemy PictureBox do wyświetlania obrazu/gifa
+            PictureBox pb = new PictureBox();
+            pb.ImageLocation = imagePath;
+            pb.SizeMode = PictureBoxSizeMode.Zoom; // Zachowuje proporcje
+            pb.Dock = DockStyle.Fill; // Wypełnia całe okno
+
+            child.Controls.Add(pb);
+            child.Show();
+        }
+    }
+}
